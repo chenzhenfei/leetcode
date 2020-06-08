99fdbc5a4e12f449b8b3af7e697b6dcfd1231df0
916927902@qq.com <916927902@qq.com>
2020-06-08 23:27:55 +0800

    1、目录结构修改

---
+++ leetcode/src/test/java/com/tsshare/leetcode/LeetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsshare.leetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
